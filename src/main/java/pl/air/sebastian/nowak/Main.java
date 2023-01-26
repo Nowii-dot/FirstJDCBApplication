7da8ccc34f4a0e2bcd1d8d02d238fa03656b67bd
Sebastian Nowak <nowaksebastian998@gmail.com>
2023-01-26 22:07:18 +0100

    Dodanie obslugi jdbc oraz klasy DatabaseService

--- src/main/java/pl/air/sebastian/nowak/Main.java
+++ src/main/java/pl/air/sebastian/nowak/Main.java
@@ -1,7 +1,16 @@
 package pl.air.sebastian.nowak;
 
+import pl.air.sebastian.nowak.db.DatabaseService;
+
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        String user = "root";
+        String password = "...";
+        String host = "127.0.0.1";
+        String database = "MYSQL";
+        DatabaseService srv = new DatabaseService(user,password,host,database);
+
     }
 }
